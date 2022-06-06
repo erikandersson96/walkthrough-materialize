3a6552bbe575c53f393503ce78d2d86dd8365a78
erikandersson96 <erik96.andersson@gmail.com>
2022-06-06 16:12:01 +0000

    Add some frontend with materialize

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-import os 
+import os
 from taskmanager import app
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP"), 
-        port=int(os.environ.get("PORT")), 
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
 
